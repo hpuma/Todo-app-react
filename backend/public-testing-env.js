a7e77e719a0cf9b36482cda649d5474daf060010
hpuma <pumahenry@live.com>
2021-01-01 03:44:27 -0500

    Added babelrc configuration and added public user credentials, db access os still unstable

---
+++ backend/public-testing-env.js
@@ -0,0 +1,9 @@
+export const ATLAS_URI="mongodb+srv://Public-user:5f8KTYMlg5RAPURT@testing.uzjty.mongodb.net/testing?retryWrites=true&w=majority";
+
+export const ACCESS_TOKEN_SECRET="14652e4cd4e2ee4df08f4eg5c5h9f84cd58b7892f73fb8c0ff2dba9e9e3aa04685cc8fa0b6204485cf4c6f3515e0dff69dc4c8835539cd47c912ba952dcec15";
+
+
+export default {
+  ATLAS_URI,
+  ACCESS_TOKEN_SECRET
+};
