e7bdcf0ccc2b44ff2e71936667458f93d1ba5687
hpuma <pumahenry@live.com>
2021-01-04 05:25:33 -0500

    Fixed access token error and updated server to be compatible with frontend

--- backend/AuthenticateToken.js
+++ backend/AuthenticateToken.js
@@ -1,11 +1,11 @@
 import jwt from "jsonwebtoken";
-
+import { ACCESS_TOKEN_SECRET } from "/public-testing-env.js";
 // Included in one of the parameters that needs a current valid log in.
-function AuthenticateToken(req, res, next){
+export function AuthenticateToken(req, res, next){
     const authHeader = req.headers['authorization'];
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null) return res.sendStatus(401);
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    jwt.verify(token, ACCESS_TOKEN_SECRET, (err, user) => {
         if(err) return res.sendStatus(403);
         req.user = user
         next();
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import routes from "routes";
 import { ATLAS_URI } from "/public-testing-env.js";
-
+import cors from "cors";
 const { loginRouter, signUpRouter, todoRouter, listRouter } = routes;
 const app = express();
 const port = 5000;
@@ -17,6 +17,7 @@ connection.once('open', () => {
 })
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/health-check', (req, res, next) => {
     res.status(200).json({ ok: 1})
@@ -30,6 +31,14 @@ app.use((req, res, next) => {
 app.get('/health-check', (req, res, next) => {
     res.status(200).json({ ok: 1})
 })
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH");
+    next();
+});
 
 app.use('/todo/login', loginRouter);
 app.use('/todo/signup', signUpRouter);
